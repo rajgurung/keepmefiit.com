20a87f09f604ff4a15d78187915c76fc958fbba1
raj <gurungraj26@gmail.com>
2016-08-01 18:03:30 +0100

    stylesheet: enhancement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts =  Post.all.collect do |post|
+    @posts =  Post.all.order(created_at: :desc).collect do |post|
                 { 
                   id: post.id,
                   title: post[:title],
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :categories
   devise_for :users
-	root to: "todos#index"
+	root to: "posts#index"
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
