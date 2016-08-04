a0da8f48c057706f9f157b4b14e8de1e202ccd3e
raj <gurungraj26@gmail.com>
2016-08-04 21:01:14 +0100

    instructin for assets added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile =  ['*.js', '*.css.map', '*.min.css', '*.css', '*.css.erb', '*.svg', '*.eot', '*.woff', '*.ttf']
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
