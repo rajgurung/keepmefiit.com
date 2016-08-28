4a02400d2dfc20bd384bc822822a4e70b087f45b
raj <gurungraj26@gmail.com>
2016-08-28 22:31:23 +0100

    only authenticated user can edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
