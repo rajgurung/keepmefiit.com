779db0b22c0824e4297cfb87f6d68adda7d3894b
rajgurung <gurungraj26@gmail.com>
2016-12-13 20:55:11 +0000

    meals.. dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  # get 'dashboards/index'
+  get "/dashboards", to: "dashboards#index"
+
   resources :meals
   resources :posts
   resources :categories
-  devise_for :users
-	root to: "posts#index"
+  devise_for :users	
   resources :todos
+
+  root to: "dashboards#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
